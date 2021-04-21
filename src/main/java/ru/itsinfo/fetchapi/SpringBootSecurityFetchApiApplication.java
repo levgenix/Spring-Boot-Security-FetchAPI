44c91fa93445f02df9b1cfa1b42c70a0d2e3eca8
levgenix <mr.lobach@gmail.com>
2021-04-21 23:48:56 +0300

    Initial commit

---
+++ src/main/java/ru/itsinfo/fetchapi/SpringBootSecurityFetchApiApplication.java
@@ -0,0 +1,13 @@
+package ru.itsinfo.fetchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityFetchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityFetchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/itsinfo/fetchapi/SpringBootSecurityFetchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itsinfo.fetchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityFetchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
